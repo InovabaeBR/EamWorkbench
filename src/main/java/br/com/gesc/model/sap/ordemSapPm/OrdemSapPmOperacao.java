5e3fe3b949ae74a9ce74823b835642dfd79ed5da
Evandro Costa Neves <ecnjava@gmail.com>
2020-07-09 15:41:23 -0300

    Catalog openings from csv files

---
+++ src/main/java/br/com/gesc/model/sap/ordemSapPm/OrdemSapPmOperacao.java
@@ -0,0 +1,21 @@
+package br.com.gesc.model.sap.ordemSapPm;
+
+import br.com.gesc.model.sap.masterData.CentroTrabalho;
+
+/**
+ * <b>Tabela SAP: AFVC</b><br>
+ *  @see OrdemSapPm
+ */
+public class OrdemSapPmOperacao {
+    public String operacao;
+    public String textoBreve;
+    public CentroTrabalho centroTrabalho;
+    public String textoLongo;
+    public Integer numeroRecursos;
+    public Double horasPlanejadas;
+
+
+
+
+
+}
